0a94cffd2b6fd3bd8752bac252dc3942ee84d978
Pooja Pandey <poojapandey8890@gmail.com>
2022-08-14 19:53:21 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, view, StatusBar } from "react-native";
 
-export default function App() {
+
+// create a component that return some jsx / simple function
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <view>
+      import React, { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
 
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect(() => {
+    fetch('https://hn.algolia.com/api/v1/search_by_date?tags=story&page=0 via')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <Text>Loading...</Text> : 
+      ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+          <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>{data.title}</Text>
+          <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Articles:</Text>
+          <FlatList
+            data={data.articles}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>{item.id + '. ' + item.title}</Text>
+            )}
+          />
+        </View>
+      )}
+      </View>
+  );
+};
+      
+
+
+// Create a StyleSheet to style the component
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  textStyle: {
+    color: "red",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
+
+// export the file, so that we can use it elsewhere in our app
+export default App;
